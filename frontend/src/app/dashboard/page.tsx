c9506c3ef2e3d446d7517b6cb400d1304d2ac416
adsy <adsy@mail.com>
2025-06-24 11:06:04 +0600

    iiii

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -277,7 +277,7 @@ export default function DashboardPage() {
                     <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <span className="font-medium">Show membership card while billing</span>
+                    <span className="font-medium">Show membership card at billing</span>
                   </div>
                 </div>                  
                 <div className="ml-4">
